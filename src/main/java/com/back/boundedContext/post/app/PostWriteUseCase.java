58b66a561a806de1525300a755e2913f64931f6d
박보영 <bunohoq@parkboyoung-ui-MacBookAir.local>
2025-12-23 17:24:15 +0900

    0011

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -4,6 +4,7 @@ import com.back.boundedContext.member.domain.Member;
 import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.EventPublisher.EventPublisher;
+import com.back.global.rsData.RsData;
 import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +23,7 @@ public class PostWriteUseCase {
         return postRepository.count();
     }
 
-    public Post write(Member author, String title, String content) {
+    public RsData<Post> write(Member author, String title, String content) {
         Post post = postRepository.save(new Post (author, title, content));
 
         eventPublisher.publish(
@@ -32,7 +33,7 @@ public class PostWriteUseCase {
         );
         //author.increaseActivityScore(3);
 
-        return post;
+        return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
     }
 
     public Optional<Post> findById(int id) {
