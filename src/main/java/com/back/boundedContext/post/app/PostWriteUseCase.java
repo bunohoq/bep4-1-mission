f3d73b6f12be8dad5d7cda52b33c9376c96b4744
박보영 <bunohoq@parkboyoung-ui-MacBookAir.local>
2025-12-24 09:27:12 +0900

    0014

--- src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
+++ src/main/java/com/back/boundedContext/post/app/PostWriteUseCase.java
@@ -6,6 +6,7 @@ import com.back.boundedContext.post.domain.Post;
 import com.back.boundedContext.post.out.PostRepository;
 import com.back.global.EventPublisher.EventPublisher;
 import com.back.global.rsData.RsData;
+import com.back.shared.member.out.MemberApiClient;
 import com.back.shared.post.dto.PostDto;
 import com.back.shared.post.event.PostCreatedEvent;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +19,7 @@ import java.util.Optional;
 public class PostWriteUseCase {
     private final PostRepository postRepository; //주입
     private final EventPublisher eventPublisher;
-    private final MemberFacade memberFacade;
+    private final MemberApiClient memberApiClient;
 
 
     public long count() {
@@ -37,7 +38,7 @@ public class PostWriteUseCase {
 
         //return new RsData<>("201-1", "%d번 글이 생성되었습니다.".formatted(post.getId()), post);
 
-        String randomSecurityTip = memberFacade.getRandomSecurityTip();
+        String randomSecurityTip = memberApiClient.getRandomSecurityTip();
 
         return new RsData<> (
                 "201-1",
