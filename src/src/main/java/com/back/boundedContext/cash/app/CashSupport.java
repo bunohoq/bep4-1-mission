d9c234eccf9130cf65f0d1fbe23661f5b73b5a08
박보영 <bunohoq@parkboyoung-ui-MacBookAir.local>
2025-12-30 12:42:53 +0900

    0036: 토스페이먼츠 오류

---
+++ src/src/main/java/com/back/boundedContext/cash/app/CashSupport.java
@@ -0,0 +1,36 @@
+package com.back.boundedContext.cash.app;
+
+import com.back.boundedContext.cash.domain.CashMember;
+import com.back.boundedContext.cash.domain.CashPolicy;
+import com.back.boundedContext.cash.domain.Wallet;
+import com.back.boundedContext.cash.out.CashMemberRepository;
+import com.back.boundedContext.cash.out.WalletRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class CashSupport {
+    //UseCase에서 사용되는 중복된 기능 모음
+    private final CashMemberRepository cashMemberRepository;
+    private final WalletRepository walletRepository;
+
+    public Optional<CashMember> findMemberByUsername(String username) {
+        return cashMemberRepository.findByUsername(username);
+    }
+
+    public Optional<Wallet> findWalletByHolder(CashMember holder) {
+        return walletRepository.findByHolder(holder);
+    }
+
+    public Optional<Wallet> findWalletByHolderId(int holderId) {
+        return walletRepository.findByHolderId(holderId);
+    }
+
+    public Optional<Wallet> findHoldingWallet() {
+        return walletRepository.findByHolderId(CashPolicy.HOLDING_MEMBER_ID);
+    }
+
+}
